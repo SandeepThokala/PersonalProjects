eff1a2595e5e643fbd5af0358fd788c9d16c3109
SandeepThokala <imdeepu98@gmail.com>
2022-02-02 01:00:51 +0530

    Uploading 3rd version Watcher

--- Watcher/Watch.py
+++ Watcher/Watch.py
@@ -1,9 +1,10 @@
 from re import search, split
-from os import listdir, startfile, chdir, system
-from os.path import isdir, join, splitext
+from os import listdir, startfile
+from os.path import isdir, join, splitext, basename
 from random import choice
 from datetime import datetime
 from csv import writer
+from subprocess import run, DEVNULL
 from shutil import move
 
 year_pattern = r"\.(\d{4})\."
@@ -12,8 +13,10 @@ get_year = lambda x: search(year_pattern, x).group(1)
 git = r"C:\Users\sande\Desktop\Deepu\MyData\PersonalProjects"
 
 def ask(message):
-  response = input(f'{message}? (Y/n): ')
-  return not(response) or (response.upper()[0] == 'Y')
+  try: response = input(f'{message}? (Y/n): ').upper()[0]
+  except IndexError: response = 'Y'
+  if response in ['Y', 'N']: return response == 'Y'
+  else: print('Invalid Input'); return ask(message)
 
 
 # ###################################################### MOVIE WATCHER ########################################################
@@ -21,12 +24,12 @@ def ask(message):
 Languages = ['FRENCH', 'CHINESE', 'KOREAN', 'JAPANESE', 'ITALIAN', 'SPANISH', 'SWEDISH', 'DANISH', 'RUSSIAN']
 
 def set_moviedata(movie_name, year):
+  print(chr(9658), movie_name, year)
   with open('MovieData.txt', 'a', newline='\n') as movie_data:
     data_writer = writer(movie_data, delimiter='\t')
     now = datetime.now()
-    data_writer .writerow([now.strftime('%b %d, %Y'), now.strftime('%I:%M %p'), movie_name, year])
-    chdir(git)
-    system(f'git add Watcher && git commit -m "{movie_name} ({year})" && git push')
+    data_writer.writerow([now.strftime('%b %d, %Y'), now.strftime('%I:%M %p'), movie_name, year])
+    # run(f'git add Watcher && git commit -m "{movie_name} ({year})" && git push', cwd = git, shell = True, stdout = DEVNULL)
 
 
 def start(inpath_arg):
@@ -36,27 +39,28 @@ def start(inpath_arg):
   try: language = list(filter(lambda x: x in Languages, tags.split('.'))).pop()
   except IndexError: language = ''
   movie_name = ' '.join([movie_name.replace('.', ' '), language]).strip()
-  print(chr(9658), movie_name, language, year)
-  set_moviedata(movie_name, year)
-  if search(rf"{movie_name}\t{year}", data):
-    print(f'Already watched {movie_name} {year}')
-    if ask('Rewatch'): startfile(join(inpath_arg, file_name))
-    else: pass
-  else: startfile(join(inpath_arg, file_name))
+  watched = search(rf"{movie_name}\t{year}", data)
+  if not(watched) or (watched and ask(f'Already watched {movie_name} {year}\nRewatch')):
+    startfile(join(inpath_arg, file_name))
+    set_moviedata(movie_name, year)
 
 
 def watch(inpath_arg):
   if isall_dir(inpath_arg):
-    data = open('MovieData.txt', 'r').read()
+    print(basename(inpath_arg))
     series = listdir(inpath_arg)
     series.sort(key = get_year)
-    for i in series: start(join(inpath_arg, i))
+    count = len(series)
+    for i, part in enumerate(series): 
+      start(join(inpath_arg, part))
+      if (count != i + 1) and ask(f'Play part {i + 2}'): continue
+      else: break; return
   else: start(inpath_arg)
-  try: return move(inpath_arg, r"E:\Watched") if ask('Move to Watched folder') else move(inpath_arg, r"E:\Delete")
+  try: return move(inpath_arg, r"E:\Watched") if ask('Move to Watched (Y) or Delete (n) folders') else move(inpath_arg, r"E:\Delete")
   except: pass
 
 
 watchlist = lambda inpath_arg: watch(join(inpath_arg, choice(listdir(inpath_arg))))
 
-watchlist(r"E:\Watchlist")
-# watch(r"E:\Watched\Spider-Man")
+# watchlist(r"E:\Watchlist")
+watch(r"E:\Watchlist\Pirates Of Caribbean")
